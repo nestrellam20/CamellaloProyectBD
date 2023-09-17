4a8ed210dbfc9e28a94202a23f8d6355bf2d966f
Nathalie Estrella <nathaliebestrellamejia@gmail.com>
2023-09-16 23:41:45 -0500

    falta

--- Proyecto-test-camellalo-main/components/usuario/storage.js
+++ Proyecto-test-camellalo-main/components/usuario/storage.js
@@ -1,42 +1,73 @@
-const model = require('./model')
+const model = require('./model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-function get_usuario( filtroUsuario ) {
-    let filtro = {}
-    if (filtroUsuario) {
-        filtro = { usuari: filtroUsuario }
+function get_usuario(filtroUsuario) {
+  let filtro = {};
+  if (filtroUsuario) {
+    filtro = { usuari: filtroUsuario };
+  }
+  return model.find(filtro);
+}
+
+function add_usuario(usuario) {
+  const objeto = new model(usuario);
+  return objeto.save();
+}
+
+async function update_usuario(usuario) {
+  const objeto = await model.findOne({ usuari: usuario.usuari });
+
+  if (objeto) {
+    objeto.nombre = usuario.nombre;
+    objeto.email = usuario.email;
+    objeto.celular = usuario.celular;
+    objeto.usuari = usuario.usuari;
+    objeto.contraseña = usuario.contraseña;
+
+    return await objeto.save();
+  } else {
+    return null;
+  }
+}
+
+async function delete_usuario(usuari) {
+  return await model.deleteOne({ usuari: usuari });
+}
+
+async function login_usuario(usuario, contraseña) {
+  try {
+    const usuarioEncontrado = await model.findOne({ usuari: usuario });
+
+    if (!usuarioEncontrado) {
+      throw new Error('Usuario no encontrado');
     }
-    const objeto = model.find( filtro )
-    return objeto
-}
 
-function add_usuario( usuario ) {
-    const objeto = new model( usuario )
-    objeto.save()
-}
+    // Verificar la contraseña utilizando bcrypt
+    const contraseñaValida = await bcrypt.compare(
+      contraseña,
+      usuarioEncontrado.contraseña
+    );
 
-async function update_usuario( usuario ) {
-    const objeto = await model.findOne( {usuari: usuario.usuari} )
-
-    if ( objeto ) {
-        objeto.nombre = usuario.nombre
-        objeto.email = usuario.email
-        objeto.celular = usuario.celular
-        objeto.usuari = usuario.usuari
-        objeto.contraseña = usuario.contraseña
-    
-        return resultado = await objeto.save()    
-    } else {
-        return null
+    if (!contraseñaValida) {
+      throw new Error('Contraseña incorrecta');
     }
-}
 
-async function delete_usuario( usuari ) {
-    return await model.deleteOne({usuari: usuari})
+    // Generar un token de autenticación con una cadena secreta directa
+    const token = jwt.sign({ id: usuarioEncontrado._id }, 'mi_secreto_secreto', {
+      expiresIn: '1d',
+    });
+
+    return { token };
+  } catch (error) {
+    throw error;
+  }
 }
 
 module.exports = {
-    add: add_usuario,
-    get: get_usuario,
-    update: update_usuario,
-    delete: delete_usuario,
-}
+  add: add_usuario,
+  get: get_usuario,
+  update: update_usuario,
+  delete: delete_usuario,
+  login: login_usuario,
+};
--- Proyecto-test-camellalo-main/public/script.js
+++ Proyecto-test-camellalo-main/public/script.js
@@ -1,91 +1,101 @@
-  document.getElementById('formulario1').addEventListener('submit', function(event) {
-  event.preventDefault(); // Evita que la página se recargue al enviar el formulario
-
-  // Obtén el valor del campo de entrada
-  var nombre = document.getElementById('nombre').value;
-  var email = document.getElementById('email').value;
-  var celular = document.getElementById('celular').value;
-  var usuario = document.getElementById('usuario').value;
-  var contraseña = document.getElementById('contraseña').value;
-
-  // Realiza una solicitud AJAX para enviar el dato
-  validateForm(nombre,email,celular,usuario,contraseña);
-  window.location.href = "index3.html";
-});
-
 document.getElementById("miBoton").addEventListener("click", function () {
-  // Redirige a la página index2.html al hacer clic en el botón "Siguiente"
-  window.location.href = "index2.html";
-});
+    // Redirige a la página index2.html al hacer clic en el botón "Siguiente"
+    window.location.href = "index2.html";
+  });
 
-document.getElementById("miBoton2").addEventListener("click", function () {
-  // Redirige a la página index2.html al hacer clic en el botón "Siguiente"
-  window.location.href = "index2.html";
-});
+  document.getElementById("miBoton2").addEventListener("click", function () {
+    // Redirige a la página index2.html al hacer clic en el botón "Siguiente"
+    window.location.href = "index2.html";
+  });
+
+  document.getElementById("miBoton3").addEventListener("click", function () {
+    // Redirige a la página index2.html al hacer clic en el botón "Siguiente"
+    window.location.href = "prueba.html";
+  });
 
 
+  // Agrega el evento submit al formulario de registro
+  document.getElementById('formulario1').addEventListener('submit', function (event) {
+    event.preventDefault(); // Evita que la página se recargue al enviar el formulario
 
-function validateForm(nombre,email,celular,usuario,contraseña) {
-  
-$.ajax({
-    url:'http://localhost:3000/usuario',
-    method:'POST',
-    data:{
-         nombre: nombre,
-         email: email,
-         celular:celular,
-         usuari:usuario,
-         contraseña:contraseña,
-    },
-    dataType:'json'
-  })
-  .then(function(data){
-    console.log('info ingresada')
-    console.log(data)
+    // Obtén el valor del campo de entrada
+    var nombre = document.getElementById('nombre').value;
+    var email = document.getElementById('email').value;
+    var celular = document.getElementById('celular').value;
+    var usuario = document.getElementById('usuario').value;
+    var contraseña = document.getElementById('contraseña').value;
+
+    // Realiza una solicitud AJAX para enviar el dato
+    validateRegistro(nombre, email, celular, usuario, contraseña);
+  });
+
+
+  function validateRegistro(nombre, email, celular, usuario, contraseña) {
+    $.ajax({
+      url: 'http://localhost:3000/usuario',
+      method: 'POST',
+      data: {
+        nombre: nombre,
+        email: email,
+        celular: celular,
+        usuari: usuario,
+        contraseña: contraseña,
+      },
+      dataType: 'json',
     })
-  .catch(function(error){
-    console.log('error');
-    console.log(error);
+      .then(function (data) {
+        console.log('Registro exitoso');
+        console.log(data);
+        window.location.href = "index3.html"; // Redirige a la página index3.html después del registro exitoso
+      })
+      .catch(function (error) {
+        console.log('Error de registro');
+        console.log(error);
+      });
+
+    }
+
+    document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById('login-form').addEventListener('submit', function (event) {
+      event.preventDefault(); // Evita que la página se recargue al enviar el formulario
+  
+      // Obtén el valor de los campos de inicio de sesión
+      var usuario = document.querySelector('input[name="usuario"]').value;
+      var contraseña = document.querySelector('input[name="contraseña"]').value;
+  
+      // Realiza una solicitud AJAX para enviar los datos de inicio de sesión
+      validateInicioSesion(usuario, contraseña);
+    });
+  
+    function validateInicioSesion(usuario, contraseña) {
+      // Realiza una solicitud AJAX al servidor para verificar las credenciales
+      $.ajax({
+        url: 'http://localhost:3000/usuario/login',
+        method: 'POST',
+        data: {
+          usuari: usuario,
+          contraseña: contraseña,
+        },
+        dataType: 'json',
+      })
+        .then(function (data) {
+          // Si las credenciales son válidas, puedes redirigir al usuario a la página deseada
+          console.log('Inicio de sesión exitoso');
+          console.log(data);
+          window.location.href = "prueba.html"; // Redirige a la página prueba.html después del inicio de sesión exitoso
+        })
+        .catch(function (error) {
+          // Si las credenciales son inválidas, puedes mostrar un mensaje de error al usuario
+          console.log('Error de inicio de sesión');
+          console.log(error.responseText);
+          alert('Credenciales inválidas. Inténtalo de nuevo.');
+        });
+    }
   });
 
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  const loginForm = document.getElementById("login-form");
-
-  loginForm.addEventListener("submit", async (event) => {
-      event.preventDefault();
-
-      const username = document.getElementById("username").value;
-      const password = document.getElementById("password").value;
-
-      // Realiza una solicitud AJAX para verificar el inicio de sesión en el servidor
-      const response = await login(username, password);
-
-      if (response.success) {
-          // Redirige al usuario a la página de inicio de sesión exitosa
-          window.location.href = "dashboard.html";
-      } else {
-          // Muestra un mensaje de error al usuario
-          alert("Nombre de usuario o contraseña incorrectos.");
-      }
-  });
-
-  // Función para realizar la solicitud de inicio de sesión al servidor
-  async function login(username, password) {
-      try {
-          const response = await fetch("/api/login", {
-              method: "POST",
-              headers: {
-                  "Content-Type": "application/json"
-              },
-              body: JSON.stringify({ username, password })
-          });
-
-          return await response.json();
-      } catch (error) {
-          console.error("Error en la solicitud de inicio de sesión:", error);
-          return { success: false };
-      }
-  }
-});
+  
+  
+  
+  
+  
+  
