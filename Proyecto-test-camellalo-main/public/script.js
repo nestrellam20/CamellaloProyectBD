1af81eeaa5cc293f478a60ad2b42c176e038481c
Nathalie Estrella <nathaliebestrellamejia@gmail.com>
2023-09-16 21:12:39 -0500

    uhm

--- Proyecto-test-camellalo-main/public/script.js
+++ Proyecto-test-camellalo-main/public/script.js
@@ -13,11 +13,17 @@
   window.location.href = "index3.html";
 });
 
+document.getElementById("miBoton").addEventListener("click", function () {
+  // Redirige a la página index2.html al hacer clic en el botón "Siguiente"
+  window.location.href = "index2.html";
+});
 
 document.getElementById("miBoton2").addEventListener("click", function () {
   // Redirige a la página index2.html al hacer clic en el botón "Siguiente"
   window.location.href = "index2.html";
 });
+
+
 
 function validateForm(nombre,email,celular,usuario,contraseña) {
   
@@ -43,3 +49,43 @@ $.ajax({
   });
 
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const loginForm = document.getElementById("login-form");
+
+  loginForm.addEventListener("submit", async (event) => {
+      event.preventDefault();
+
+      const username = document.getElementById("username").value;
+      const password = document.getElementById("password").value;
+
+      // Realiza una solicitud AJAX para verificar el inicio de sesión en el servidor
+      const response = await login(username, password);
+
+      if (response.success) {
+          // Redirige al usuario a la página de inicio de sesión exitosa
+          window.location.href = "dashboard.html";
+      } else {
+          // Muestra un mensaje de error al usuario
+          alert("Nombre de usuario o contraseña incorrectos.");
+      }
+  });
+
+  // Función para realizar la solicitud de inicio de sesión al servidor
+  async function login(username, password) {
+      try {
+          const response = await fetch("/api/login", {
+              method: "POST",
+              headers: {
+                  "Content-Type": "application/json"
+              },
+              body: JSON.stringify({ username, password })
+          });
+
+          return await response.json();
+      } catch (error) {
+          console.error("Error en la solicitud de inicio de sesión:", error);
+          return { success: false };
+      }
+  }
+});
